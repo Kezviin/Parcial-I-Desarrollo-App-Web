0abd47fc845234708ef512c782d33fe38cc0c596
kevin <kevinlibardorosas@gmail.com>
2026-03-20 15:59:36 -0500

    Primer commit del login

---
+++ js/login.js
@@ -0,0 +1,19 @@
+// Credenciales quemadas solo para fines educativos
+var usuarioCorrecto = "admin";
+var contrasenaCorrecto = "1234";
+
+var boton = document.getElementById("btn-login");
+
+boton.addEventListener("click", function() {
+
+    var usuario = document.getElementById("usuario").value;
+    var contrasena = document.getElementById("contrasena").value;
+    var mensajeError = document.getElementById("mensaje-error");
+
+    if (usuario === usuarioCorrecto && contrasena === contrasenaCorrecto) {
+        window.location.href = "index.html";
+    } else {
+        mensajeError.textContent = "Usuario o contraseña incorrectos";
+    }
+
+});
